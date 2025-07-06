70b9a00ae91e7f4daa677c40a9703de36fc675b5
Laurence Stokes <laurence_stokes@hotmail.com>
2025-07-06 13:02:28 +0100

    chore: change pre-commit hook from linting to validation

---
+++ types/jest-dom.d.ts
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom'
+
+declare global {
+    namespace jest {
+        interface Matchers<R> {
+            toBeInTheDocument(): R
+            toHaveClass(...classNames: string[]): R
+            toHaveAttribute(attr: string, value?: string): R
+            toHaveTextContent(text: string | RegExp): R
+            toBeVisible(): R
+            toBeDisabled(): R
+            toBeEnabled(): R
+            toBeChecked(): R
+            toBePartiallyChecked(): R
+            toHaveValue(value: string | string[] | number): R
+            toHaveDisplayValue(value: string | RegExp | (string | RegExp)[]): R
+            toBeRequired(): R
+            toBeInvalid(): R
+            toBeValid(): R
+            toHaveAccessibleName(name: string | RegExp): R
+            toHaveAccessibleDescription(description: string | RegExp): R
+            toHaveStyle(css: string | Record<string, any>): R
+            toHaveFocus(): R
+            toHaveFormValues(expectedValues: Record<string, any>): R
+            toHaveElementClass(className: string): R
+            toHaveAttribute(attr: string, value?: string): R
+        }
+    }
+}
+
+export { } 
