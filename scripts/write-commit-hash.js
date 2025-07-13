4c063e9c73ec79a5f8e068926de800840b871b76
Laurence Stokes <laurence_stokes@hotmail.com>
2025-07-13 15:34:27 +0100

    chore: lint fixes

--- scripts/write-commit-hash.js
+++ scripts/write-commit-hash.js
@@ -6,12 +6,12 @@ const commitHash = execSync('git rev-parse HEAD').toString().trim();
 const envPath = '.env.local';
 let envContent = '';
 if (fs.existsSync(envPath)) {
-    envContent = fs.readFileSync(envPath, 'utf8');
-    // Remove any existing NEXT_PUBLIC_COMMIT_HASH line
-    envContent = envContent.replace(/^NEXT_PUBLIC_COMMIT_HASH=.*$/m, '');
-    envContent = envContent.trim() + '\n';
+  envContent = fs.readFileSync(envPath, 'utf8');
+  // Remove any existing NEXT_PUBLIC_COMMIT_HASH line
+  envContent = envContent.replace(/^NEXT_PUBLIC_COMMIT_HASH=.*$/m, '');
+  envContent = envContent.trim() + '\n';
 }
 envContent += `NEXT_PUBLIC_COMMIT_HASH=${commitHash}\n`;
 fs.writeFileSync(envPath, envContent);
 
-console.log('Wrote commit hash:', commitHash); 
+console.log('Wrote commit hash:', commitHash);
